aca6e2b8026059ee83a615c9b7f17505a54871bc
Giorgi Kapanadze <kapanadze.g@sa.gov.ge>
2025-06-23 13:46:05 +0400

    Deploy to GitHub Pages

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import SEO from './seo/SEO';
+
+function Home() {
+  return (
+    <div>
+      <SEO title="Home | SEO React App" description="საძიებო სისტემის ოპტიმიზაცია React ბიბლიოთეკაში" />
+      <h1>საძიებო სისტემის ოპტიმიზაცია React ბიბლიოთეკაში</h1>
+      <p>This content is visible to search engines after JS is rendered.</p>
+    </div>
+  );
+}
+
+export default Home;
