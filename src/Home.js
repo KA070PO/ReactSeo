b1bdc08d5941dddecafe721e66c3c02964fe7a25
KA070PO <76748409+KA070PO@users.noreply.github.com>
2025-06-24 15:47:40 +0400

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div>
       <SEO title="Home | SEO React App" description="საძიებო სისტემის ოპტიმიზაცია React ბიბლიოთეკაში" />
-      <h1>საძიებო სისტემის ოპტიმიზაცია React ბიბლიოთეკაში</h1>
+      <h1>საძიებო სისტემის ოპტიმიზაცია React ბიბლიოთეკაში სატესტო გარემო</h1>
       <p>This content is visible to search engines after JS is rendered.</p>
     </div>
   );
